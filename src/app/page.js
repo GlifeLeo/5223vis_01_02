3d5242e6f1146115b9c6a9aa68e3cc4ffa95b9c0
Leo <leo.pham@glife.com.sg>
2025-03-08 08:56:46 +0700

    edit layout

--- src/app/page.js
+++ src/app/page.js
@@ -6,12 +6,6 @@ import BestSeller from "@/app/components/BestSeller"
 function Home() {
   return (
     <div>
-      <div className='bg-gray-600 py-12'>
-        <div className='w-4/5 mx-auto'>
-          <TopBar></TopBar>
-          <Categories></Categories>
-        </div>
-      </div>
       <div className='w-4/5 mx-auto'>
         <BestSeller></BestSeller>
       </div>
